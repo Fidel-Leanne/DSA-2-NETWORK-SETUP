6aa21e81f450cf365f80c81505a009d9d3fc5531
Jamey Rittmann <jameyleerittmann@gmail.com>
2024-04-24 20:01:04 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create a star network
+        Star starNetwork = new Star();
+
+        // Insert client nodes into the network
+        starNetwork.insertNode("Client1");
+        starNetwork.insertNode("Client2");
+        starNetwork.insertNode("Client3");
+
+        // Get references to client nodes from the star network
+        ClientNode client1 = starNetwork.getClientNode("Client1");
+        ClientNode client2 = starNetwork.getClientNode("Client2");
+        ClientNode client3 = starNetwork.getClientNode("Client3");
+
+        // Send messages with Huffman codes
+        String message1 = "Hello from Client 1!";
+        String message2 = "Hello from Client 2!";
+        String message3 = "Hello from Client 3!";
+
+        // Ensure Huffman tree is built before sending messages
+        client1.sendWithHuffmanCodes(message1);
+        client2.sendWithHuffmanCodes(message2);
+        client3.sendWithHuffmanCodes(message3);
     }
 }
